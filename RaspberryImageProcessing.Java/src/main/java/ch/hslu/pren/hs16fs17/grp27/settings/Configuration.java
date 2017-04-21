54d840a92a6b0653f267605cf971b895e0b74532
Silvio <silvio.stappung@hotmail.com>
2017-04-21 09:12:11 +0200

    Configured GPIO Pin Configured according to Schema

--- RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/settings/Configuration.java
+++ RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/settings/Configuration.java
@@ -18,14 +18,14 @@ public class Configuration {
     public static final Pin BIT2SIGNALPIN = RaspiPin.GPIO_03;
     public static final Pin BIT3SIGNALPIN = RaspiPin.GPIO_04;
 
-    public static final Pin DISPLAYDOTPIN = RaspiPin.GPIO_29;
+    public static final Pin DISPLAYDOTPIN = RaspiPin.GPIO_27;
     public static final Pin DISPLAYTOPMIDPIN = RaspiPin.GPIO_21;
-    public static final Pin DISPLAYTOPLEFTPIN = RaspiPin.GPIO_22;
-    public static final Pin DISPLAYTOPRIGHTPIN = RaspiPin.GPIO_23;
-    public static final Pin DISPLAYMIDPIN = RaspiPin.GPIO_24;
-    public static final Pin DISPLAYBOTTOMMIDPIN = RaspiPin.GPIO_25;
-    public static final Pin DISPLAYBOTTOMLEFTPIN = RaspiPin.GPIO_26;
-    public static final Pin DISPLAYBOTTOMRIGHTPIN = RaspiPin.GPIO_27;
+    public static final Pin DISPLAYTOPLEFTPIN = RaspiPin.GPIO_28;
+    public static final Pin DISPLAYTOPRIGHTPIN = RaspiPin.GPIO_22;
+    public static final Pin DISPLAYMIDPIN = RaspiPin.GPIO_29;
+    public static final Pin DISPLAYBOTTOMMIDPIN = RaspiPin.GPIO_24;
+    public static final Pin DISPLAYBOTTOMLEFTPIN = RaspiPin.GPIO_25;
+    public static final Pin DISPLAYBOTTOMRIGHTPIN = RaspiPin.GPIO_23;
 
 
 }
