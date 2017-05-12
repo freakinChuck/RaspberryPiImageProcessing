45e84e72dd547aa5405babd7a0e48cea37bbd791
SamuelTenga <samueltenga@gmail.com>
2017-05-12 09:50:37 +0200

    PageMode set to single word. Character whitelist disabled.

--- RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/imageprocessing/helper/TesseractInstance.java
+++ RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/imageprocessing/helper/TesseractInstance.java
@@ -1,5 +1,6 @@
 package ch.hslu.pren.hs16fs17.grp27.imageprocessing.helper;
 
+import net.sourceforge.tess4j.ITessAPI;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 
@@ -19,7 +20,8 @@ public class TesseractInstance {
     protected TesseractInstance(){
         singletonTesseract = new Tesseract();
         singletonTesseract.setDatapath(System.getenv("TESSDATA_PREFIX"));
-        singletonTesseract.setTessVariable("tessedit_char_whitelist", "IV");
+        //singletonTesseract.setTessVariable("tessedit_char_whitelist", "IV"); //--Works sometimes
+        singletonTesseract.setPageSegMode(ITessAPI.TessPageSegMode.PSM_SINGLE_WORD);
     }
 
 
