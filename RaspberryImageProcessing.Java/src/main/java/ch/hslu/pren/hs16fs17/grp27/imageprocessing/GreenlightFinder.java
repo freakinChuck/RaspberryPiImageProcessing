9df1ffa58ca2ed8bb85490f9d097c3d04b75b69d
SamuelTenga <samueltenga@gmail.com>
2017-05-25 10:57:29 +0200

    new Red/Green- range and circle parameter

--- RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/imageprocessing/GreenlightFinder.java
+++ RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/imageprocessing/GreenlightFinder.java
@@ -27,22 +27,22 @@ public class GreenlightFinder {
             Imgproc.cvtColor(frame, hsv_image, Imgproc.COLOR_BGR2HSV);
             Mat green_hue_range = new Mat();
             //Set Green range
-            int sensitivity = 20;
-            Core.inRange(hsv_image, new Scalar(60 - sensitivity, 100, 100), new Scalar(60 + sensitivity, 255, 255), green_hue_range);
+            int sensitivity = 10;
+            Core.inRange(hsv_image, new Scalar(60 - sensitivity, 120, 60), new Scalar(60 + sensitivity, 255, 240), green_hue_range);
             //Slightly blur the image. Improves false-positiv circle detection
             Imgproc.GaussianBlur(green_hue_range, green_hue_range, new Size(9, 9), 2, 2);
 
             // Find Cirlces
             // -Settings
             Mat circles = new Mat();
-            int iCannyUpperThreshold = 10;
+            int iCannyUpperThreshold = 60;
             int iMinRadius = 10;
             int iMaxRadius = 20;
-            int iAccumulator = 10;
+            int iAccumulator = 30;
 
             //Find Circles with Hough algo.
             Imgproc.HoughCircles(green_hue_range, circles, Imgproc.CV_HOUGH_GRADIENT,
-                    1.0, green_hue_range.rows(), iCannyUpperThreshold, iAccumulator,
+                    1.0, green_hue_range.rows()/8, iCannyUpperThreshold, iAccumulator,
                     iMinRadius, iMaxRadius);
             if (circles.cols() > 0) {
                 for (int x = 0; x < circles.cols(); x++) {
