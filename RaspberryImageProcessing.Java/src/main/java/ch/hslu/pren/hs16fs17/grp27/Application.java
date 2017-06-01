c07941894cddedc83e1911006d84ed4638047100
Silvio <silvio.stappung@hotmail.com>
2017-06-01 16:24:50 +0200

    Exception Fallback

--- RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/Application.java
+++ RaspberryImageProcessing.Java/src/main/java/ch/hslu/pren/hs16fs17/grp27/Application.java
@@ -17,6 +17,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -36,256 +37,288 @@ public class Application {
 
     }
 
-    public static void main(String[] args) throws InterruptedException {
-
-        GpioCommunication communication;
-
-        if (Configuration.DOCOMMUNICATION && Configuration.DOWAITFORARDUINO) {
-            System.out.println("Waiting for Arduino");
-
-            communication = new GpioCommunication();
-            communication.ClearPinData();
-
-            communication.DisplayTilt();
-            while (!communication.IsArduinoReady()){
-                Thread.sleep(100);
-            }
-            communication.ClearPinData();
-            communication.InitiateWaitState();
-
-            System.out.println("Arduino Ready");
-
-        }
-        else if (Configuration.DOCOMMUNICATION) {
-            communication = new GpioCommunication();
-            communication.ClearPinData();
-            communication.InitiateWaitState();
-
-            System.out.println("ArduinoWait Deactivated");
-
-        }
-
-
-        Application.SetUpEnvironment();
-
-        Camera frontCamera = new Camera(Configuration.FRONTCAMERAINDEX);
-        RedLightHeight redLightHeight = new RedLightHeight();
-        GreenlightFinder greenlightFinder = new GreenlightFinder();
-        RedBarFinder redBarFinder = new RedBarFinder();
-        RomanCharacterFinder romanCharacterFinder = new RomanCharacterFinder();
-        int frontCameraWidth;
-        int frontCameraHeight;
-
-        int count = 0;
-        while (!frontCamera.Available() && count++ < 20){
-            Thread.sleep(250);
-            System.out.println("Camera not Available");
-
-        }
-
-        if (!frontCamera.Available()){
-            System.out.println("TILT.....");
-            if (Configuration.DOCOMMUNICATION)
-                communication.DisplayTilt();
-            Thread.sleep(2000);
-
-            System.out.println("Shutdown initiated....");
-            if (Configuration.DOCOMMUNICATION)
-                communication.ClearPinData();
-            return;
-        }
-
-        //Save Camera With and Height for further steps
-        frontCameraWidth = frontCamera.Capture().width();
-        frontCameraHeight = frontCamera.Capture().height();
+    public static void main(String[] args) throws InterruptedException, IOException {
 
         String folderBasePath = getFolderBaseBath();
-        if (Files.notExists(Paths.get(folderBasePath))) {
-            new File(folderBasePath).mkdirs();
-        }
-
-
-        if (Configuration.DOCOMMUNICATION)
-            communication.DisplayRedlightWait();
-
-        Rect upperHalfOfImage = new Rect(0,frontCameraHeight/3,frontCameraWidth,frontCameraHeight/3*2);
-        while(!redLightHeight.FindRedLightHeight(new Mat(frontCamera.Capture(),upperHalfOfImage))){
-            System.out.println("waiting for Redlight");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-            }
-        }
-
-
-        System.out.println("Redlight found at: " + redLightHeight.getRedXPos());
-        if (Configuration.DOCOMMUNICATION)
-            communication.DisplayGreenlightWait();
-
-        //Rect belowRedLight = new Rect(redLightHeight.getRedXPos(), 0, frontCameraWidth-redLightHeight.getRedXPos(),frontCameraHeight);
-        Rect belowRedLight = new Rect(0,0,redLightHeight.getRedXPos(),frontCameraWidth);
-
-        while (!greenlightFinder.ImageContainsGreenLight(frontCamera.Capture())){
-            System.out.println("waiting for Greenlight");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-
-
-            }
-        }
-        
-        System.out.println("Green light Found");
-        frontCamera.Shutdown();
-
-        if (Configuration.DOCOMMUNICATION)
-            communication.DisplayGo();
-
-        if (Configuration.DOCOMMUNICATION)
-            communication.SendStartSignal();
-
-        System.out.println("Start Signal sent to Arduino");
-
 
 
         try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        Camera sideCamera = new Camera(Configuration.SIDECAMERAINDEX);
+            GpioCommunication communication;
 
-        System.out.println("Starting Number Recognition");
-        List<Future<Integer>> processList = new ArrayList<>();
-        List<Integer> intList = new ArrayList<>();
+            if (Configuration.DOCOMMUNICATION && Configuration.DOWAITFORARDUINO) {
+                System.out.println("Waiting for Arduino");
 
-        boolean redBarFound = false;
+                communication = new GpioCommunication();
+                communication.ClearPinData();
 
-        final int numberOfImagesToTake = 12;
-
-        Image[] redBarImages = new Image[numberOfImagesToTake];
-
-        while (redBarImages[0] == null) {
-            Mat capturedImage = sideCamera.Capture();
-            Image foundImage = redBarFinder.FindRedDoubleBar(capturedImage, 0);
-            if (foundImage != null) {
-                System.out.println("Redbar found");
-                redBarImages[0] = foundImage;
-            }
-            System.gc();
-        }
-
-        Mat[] matrixesToEvaluate = new Mat[numberOfImagesToTake -1];
-        for (int i = 0; i < matrixesToEvaluate.length; i++) {
-            System.out.println("Picture taken" + i);
-            matrixesToEvaluate[i] = sideCamera.Capture();
-        }
-
-        if (Configuration.DOCOMMUNICATION)
-            communication.DisplayRedbarFound();
-
-        for (int i = 0; i < matrixesToEvaluate.length; i++) {
-            redBarImages[i+1] = redBarFinder.FindRedDoubleBar(matrixesToEvaluate[i], i);
-            if (redBarImages[i+1] != null) {
-                System.out.println("RedbarFound Picture" + i);
-            }
-            else {
-                System.out.println("RedbarFailed Picture" + i);
-            }
-        }
-
-        for (int x = 0; x < numberOfImagesToTake; x++) {
-
-            final int index = x;
-            Future<Integer> numberRecognitionFuture = CompletableFuture.completedFuture(redBarImages[x])
-                    .thenApplyAsync(img -> img != null ? romanCharacterFinder.FindCharacter(img, index) : 0)
-                    .thenApplyAsync(i -> {
-                        if (i > 0)
-                            intList.add(i);
-                        return i;
-                    });
-
-            processList.add(numberRecognitionFuture);
-        }
-
-
-        for (Future<Integer> future:
-                processList){
-            try {
-                System.out.println("Erkennte Zahl: " + future.get());
-            }
-            catch(Exception ex){
-                ex.printStackTrace();
-            }
-        }
-
-
-        Object[] objects =  processList.stream().map(f -> {
-            try {
-                int number = f.get();
-                number  = number == 6 ? 4 : number;
-                return number;
-            }
-            catch (Exception exc)
-            {
-                exc.printStackTrace();
-                return 0;
-            }
-        }).toArray();
-
-        Map<Integer, Long> numbers = new HashMap<>();
-        for (int i = 1; i <= 5; i++) {
-            final int currentEvaluationNumber = i;
-            long numberCount = Arrays.stream(objects).filter(o -> (int) o == currentEvaluationNumber).count();
-            numbers.put(currentEvaluationNumber, numberCount);
-        }
-
-        int maxKey = 1;
-        long maxNumbers = numbers.get(1);
-        for (int i = 2; i <= 5; i++) {
-            long currentCount = numbers.get(i);
-            if (maxNumbers < currentCount){
-                maxKey = i;
-                maxNumbers = currentCount;
-            }
-        }
-
-
-        System.out.println("Number "+ maxKey + " found");
-
-        if (Configuration.DOCOMMUNICATION) {
-
-            communication.DisplayNumber(maxKey);
-            communication.SendNumber(maxKey);
-        }
-
-        System.out.println("Number " + maxKey + " sent to Arduino");
-
-
-        PrintNumberInConsole(maxKey, maxNumbers, objects.length);
-
-        if (Configuration.DOTAKEIMAGES) {
-            for (int i = 0; i < matrixesToEvaluate.length; i++) {
-                Mat mat = matrixesToEvaluate[i];
-                MatToBufImg matToBufImg = new MatToBufImg();
-                matToBufImg.setMatrix(mat, ".png");
-
-                File outputfile = new File(folderBasePath + "/" + "Original" + String.format("%03d", i)+ ".png");
-                try {
-                    ImageIO.write(matToBufImg.getBufferedImage(), "png", outputfile);
-                    System.out.println("Wrote File: " + outputfile.getAbsolutePath());
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                communication.DisplayTilt();
+                while (!communication.IsArduinoReady()) {
+                    Thread.sleep(100);
                 }
+                communication.ClearPinData();
+                communication.InitiateWaitState();
+
+                System.out.println("Arduino Ready");
 
             }
-            System.out.println("File write done");
+            else if (Configuration.DOCOMMUNICATION) {
+                communication = new GpioCommunication();
+                communication.ClearPinData();
+                communication.InitiateWaitState();
+
+                System.out.println("ArduinoWait Deactivated");
+
+            }
+
+
+            Application.SetUpEnvironment();
+
+            Camera frontCamera = new Camera(Configuration.FRONTCAMERAINDEX);
+            RedLightHeight redLightHeight = new RedLightHeight();
+            GreenlightFinder greenlightFinder = new GreenlightFinder();
+            RedBarFinder redBarFinder = new RedBarFinder();
+            RomanCharacterFinder romanCharacterFinder = new RomanCharacterFinder();
+            int frontCameraWidth;
+            int frontCameraHeight;
+
+            int count = 0;
+            while (!frontCamera.Available() && count++ < 20) {
+                Thread.sleep(250);
+                System.out.println("Camera not Available");
+
+            }
+
+            if (!frontCamera.Available()) {
+                System.out.println("TILT.....");
+                if (Configuration.DOCOMMUNICATION)
+                    communication.DisplayTilt();
+                Thread.sleep(2000);
+
+                System.out.println("Shutdown initiated....");
+                if (Configuration.DOCOMMUNICATION)
+                    communication.ClearPinData();
+                return;
+            }
+
+            //Save Camera With and Height for further steps
+            frontCameraWidth = frontCamera.Capture().width();
+            frontCameraHeight = frontCamera.Capture().height();
+
+            if (Files.notExists(Paths.get(folderBasePath))) {
+                new File(folderBasePath).mkdirs();
+            }
+
+
+            if (Configuration.DOCOMMUNICATION)
+                communication.DisplayRedlightWait();
+
+            Rect upperHalfOfImage = new Rect(0, frontCameraHeight / 3, frontCameraWidth, frontCameraHeight / 3 * 2);
+            while (!redLightHeight.FindRedLightHeight(new Mat(frontCamera.Capture(), upperHalfOfImage))) {
+                System.out.println("waiting for Redlight");
+                try {
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException e) {
+                }
+            }
+
+
+            System.out.println("Redlight found at: " + redLightHeight.getRedXPos());
+            if (Configuration.DOCOMMUNICATION)
+                communication.DisplayGreenlightWait();
+
+            //Rect belowRedLight = new Rect(redLightHeight.getRedXPos(), 0, frontCameraWidth-redLightHeight.getRedXPos(),frontCameraHeight);
+            Rect belowRedLight = new Rect(0, 0, redLightHeight.getRedXPos(), frontCameraWidth);
+
+            while (!greenlightFinder.ImageContainsGreenLight(frontCamera.Capture())) {
+                System.out.println("waiting for Greenlight");
+                try {
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException e) {
+
+
+                }
+            }
+
+            System.out.println("Green light Found");
+            frontCamera.Shutdown();
+
+            if (Configuration.DOCOMMUNICATION)
+                communication.DisplayGo();
+
+            if (Configuration.DOCOMMUNICATION)
+                communication.SendStartSignal();
+
+            System.out.println("Start Signal sent to Arduino");
+
+
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Camera sideCamera = new Camera(Configuration.SIDECAMERAINDEX);
+
+            System.out.println("Starting Number Recognition");
+            List<Future<Integer>> processList = new ArrayList<>();
+            List<Integer> intList = new ArrayList<>();
+
+            boolean redBarFound = false;
+
+            final int numberOfImagesToTake = 12;
+
+            Image[] redBarImages = new Image[numberOfImagesToTake];
+
+            while (redBarImages[0] == null) {
+                Mat capturedImage = sideCamera.Capture();
+                Image foundImage = redBarFinder.FindRedDoubleBar(capturedImage, 0);
+                if (foundImage != null) {
+                    System.out.println("Redbar found");
+                    redBarImages[0] = foundImage;
+                }
+                System.gc();
+            }
+
+            Mat[] matrixesToEvaluate = new Mat[numberOfImagesToTake - 1];
+            for (int i = 0; i < matrixesToEvaluate.length; i++) {
+                System.out.println("Picture taken" + i);
+                matrixesToEvaluate[i] = sideCamera.Capture();
+            }
+
+            if (Configuration.DOCOMMUNICATION)
+                communication.DisplayRedbarFound();
+
+            for (int i = 0; i < matrixesToEvaluate.length; i++) {
+                redBarImages[i + 1] = redBarFinder.FindRedDoubleBar(matrixesToEvaluate[i], i);
+                if (redBarImages[i + 1] != null) {
+                    System.out.println("RedbarFound Picture" + i);
+                }
+                else {
+                    System.out.println("RedbarFailed Picture" + i);
+                }
+            }
+
+            for (int x = 0; x < numberOfImagesToTake; x++) {
+
+                final int index = x;
+                Future<Integer> numberRecognitionFuture = CompletableFuture.completedFuture(redBarImages[x])
+                        .thenApplyAsync(img -> img != null ? romanCharacterFinder.FindCharacter(img, index) : 0)
+                        .thenApplyAsync(i -> {
+                            if (i > 0)
+                                intList.add(i);
+                            return i;
+                        });
+
+                processList.add(numberRecognitionFuture);
+            }
+
+
+            for (Future<Integer> future : processList) {
+                try {
+                    System.out.println("Erkennte Zahl: " + future.get());
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+
+            Object[] objects = processList.stream().map(f -> {
+                try {
+                    int number = f.get();
+                    number = number == 6 ? 4 : number;
+                    return number;
+                }
+                catch (Exception exc) {
+                    exc.printStackTrace();
+                    return 0;
+                }
+            }).toArray();
+
+            Map<Integer, Long> numbers = new HashMap<>();
+            for (int i = 1; i <= 5; i++) {
+                final int currentEvaluationNumber = i;
+                long numberCount = Arrays.stream(objects).filter(o -> (int) o == currentEvaluationNumber).count();
+                numbers.put(currentEvaluationNumber, numberCount);
+            }
+
+            int maxKey = 1;
+            long maxNumbers = numbers.get(1);
+            for (int i = 2; i <= 5; i++) {
+                long currentCount = numbers.get(i);
+                if (maxNumbers < currentCount) {
+                    maxKey = i;
+                    maxNumbers = currentCount;
+                }
+            }
+
+
+            System.out.println("Number " + maxKey + " found");
+
+            if (Configuration.DOCOMMUNICATION) {
+
+                communication.DisplayNumber(maxKey);
+                communication.SendNumber(maxKey);
+            }
+
+            System.out.println("Number " + maxKey + " sent to Arduino");
+
+
+            PrintNumberInConsole(maxKey, maxNumbers, objects.length);
+
+            if (Configuration.DOTAKEIMAGES) {
+                for (int i = 0; i < matrixesToEvaluate.length; i++) {
+                    Mat mat = matrixesToEvaluate[i];
+                    MatToBufImg matToBufImg = new MatToBufImg();
+                    matToBufImg.setMatrix(mat, ".png");
+
+                    File outputfile = new File(folderBasePath + "/" + "Original" + String.format("%03d", i) + ".png");
+                    try {
+                        ImageIO.write(matToBufImg.getBufferedImage(), "png", outputfile);
+                        System.out.println("Wrote File: " + outputfile.getAbsolutePath());
+
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                System.out.println("File write done");
+            }
+
+            Thread.sleep(10000);
+
         }
+        catch (Exception exc) {
 
-        Thread.sleep(10000);
+            if (Configuration.DOCOMMUNICATION) {
+                GpioCommunication communication = new GpioCommunication();
+                communication.DisplayTilt();
 
+                Thread.sleep(5000);
+
+                communication.SendNumber(4);
+                communication.DisplayNumber(4);
+            }
+
+            File outputfile = new File(folderBasePath + "/" + "Error.txt");
+
+            if (!outputfile.exists()) {
+                outputfile.createNewFile();
+            }
+
+            PrintWriter out = new PrintWriter(outputfile);
+            out.print("Exception of Type: ");
+            out.println(exc.getClass().getName());
+            out.println(exc.getMessage());
+            exc.printStackTrace(out);
+
+            throw exc;
+        }
     }
 
     public static void SetUpEnvironment(){
